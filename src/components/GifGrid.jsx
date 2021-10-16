a26e4bbcd4fda32bcd8d21f64019c7b71fda0c46
Oscar Menacho <58416396+Omr30@users.noreply.github.com>
2021-10-16 13:45:25 -0500

    FirstComment-10-2021

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
+
+export const GifGrid = ({ category }) => {
+
+    const [images, setImages] = useState([])
+
+    useEffect( () => {
+        getGifs();
+    }, [])
+
+    const getGifs = async() => {
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=e4t3GIXS67fmp3tXvhcppf9GPeqRSDM6'
+        const resp = await fetch( url );
+        const {data} = await resp.json();
+
+        const gifs = data.map ( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images?.downsized_medium.url
+            }
+        })
+
+        console.log(gifs);
+        setImages( gifs )
+    }
+
+    return (
+        <div>
+            <h3> { category } </h3>
+
+                {
+                    images.map( img => (
+                        <GifGridItem
+                            key={img.id}
+                            { ...img }
+                        />
+                    ))  
+                }
+
+        </div>
+    )
+}
