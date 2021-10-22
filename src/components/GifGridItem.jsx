7c0e44b1cd8626aaca49afe577d755fe134e3f42
Oscar Menacho <58416396+Omr30@users.noreply.github.com>
2021-10-22 03:14:40 -0500

    Pruebas del componente - GifGridItem

--- src/components/GifGridItem.jsx
+++ src/components/GifGridItem.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export const GifGridItem = ( {id, title, url} ) => {
+
+export const GifGridItem = ( {title, url} ) => {
 
 
     return (
@@ -9,4 +11,10 @@ export const GifGridItem = ( {id, title, url} ) => {
             <p> { title } </p>
         </div>
     )
+}
+
+
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 }
---
+++ src/test/components/GifGridItem.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { GifGridItem } from '../../components/GifGridItem';
+const { shallow } = require("enzyme");
+
+describe('Pruebas en <GifGridItem />', () => {
+
+    const title = 'Un titulo';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow( <GifGridItem title={title} url={url} /> )
+    
+    test('debe de mostrar el componente corractemente', () => {
+
+        expect( wrapper ).toMatchSnapshot();
+
+    });
+
+    test('debe de tener un parrafo con el titulo', () => {
+        
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe( title );
+
+    });
+    
+    test('debe de tener la imagen igual al url y alt ded los porps', () => {
+        
+        const img = wrapper.find('img');
+
+        expect( img.prop('src') ).toBe(url);
+        expect( img.prop('alt') ).toBe(title);
+
+    });
+    
+    test('debe tener animate__flipInX', () => {
+        
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect(className.includes('animate__flipInX') ).toBe( true );
+
+    })
+    
+
+
+})
